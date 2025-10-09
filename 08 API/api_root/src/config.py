07273d09b82d54bfb04c32a4245a1a84584573d0
dabenedetto <dabenedetto@usnh.edu>
2025-10-09 09:14:50 -0400

    asdf

--- 08 API/api_root/src/config.py
+++ 08 API/api_root/src/config.py
@@ -3,7 +3,7 @@ class USER_API_CONFIG:
 
 class TEST_CONFIG:
 
-    # DB_URL = "mongodb+srv://student123:jUzoD22RHBqgvr8z@cluster0.n5ntvo6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    # DB_URL = "<YOUR ATLAS URL>"
     DB_URL = "mongodb://localhost:27017"
 
     TEST_DB = "test_db"
